fd5f1b85d77ffaf4c594807ca95f7c0b4f81285c
Madelyn Andrews <maddya30@email.unc.edu>
2021-02-11 11:40:51 -0500

    Progress on Exercise 2 P1

--- exercises/ex02_p1/fortune_cookie.py
+++ exercises/ex02_p1/fortune_cookie.py
@@ -2,18 +2,31 @@
 
 from random import randint
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730393750"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Your fortune cookie says...")
     # TODO 2: Print the result of calling your fortune_cookie function.
+    print(fortune_cookie())
     print("Now, go spread positive vibes!")
 
-
 # TODO 1: Define your fortune_cookie function here.
-
+def fortune_cookie() -> str:
+    fortune: int = randint(1, 4)
+    if fortune == 1:
+        return ("Love is headed your way!")
+    else:
+        if fortune == 2:
+            return ("Your hard work will pay off soon!")
+        else:
+            if fortune == 3:
+                return ("A pleasant surprise is in store for you this week!")
+            else:
+                if fortune == 4:
+                    return ("Tomorrow will be a great day to try something new!")
+    return str()
 
 # Python Idiom for "starting" the program when run as a module.
 # The special dunder variable __name__ will be "__main__" when run as module. 
--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -1,16 +1,27 @@
 """Tar Heels exercise redux as a structured program."""
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730393750"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
     # TODO 2: Print the response of calling the tar_heels function here.
-
+    print(tar_heels(choice))
+    return None
 
 # TODO 1: Define the tar_heels function, and its logic, here.
-
+def tar_heels(x: int) -> str:
+    if x // 2 == x / 2 and x // 7 == x / 7:
+        return ("TAR HEELS")
+    else:
+        if x // 2 == x / 2:
+            return ("TAR")
+        else:
+            if x // 7 == x / 7:
+                return ("HEELS")
+            else:
+                return ("CAROLINA")
 
 if __name__ == "__main__":
     main()
